5bab815f79a8cef8a880ab550ff8000fdb3a6a4d
bridgerbird <bridgerjbird@gmail.com>
2026-07-23 17:00:56 -0600

    Refactored Camera which also fixed zooming in and out behavior

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -11,24 +11,37 @@ var initial_zoom_flag = true
 # Timer variables
 var idle_timer = 0.0
 
+# Target variables
+var target_zoom = zoom_in
+var target_speed = GameConfig.initial_lerp_speed
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# Zoom out from initial zoom to zoom_in variable
 	if initial_zoom_flag:
-		zoom = zoom.lerp(zoom_in, GameConfig.initial_lerp_speed)
+		target_zoom = zoom_in
+		target_speed = GameConfig.initial_lerp_speed
 		if zoom <= zoom_in:
 			initial_zoom_flag = false
 	
 	# Handle Zoom Out for faster speeds
-	if abs(get_parent().velocity.x) >= GameConfig.move_speed * 0.6 && zoom > zoom_out:
-		zoom = zoom.lerp(zoom_out, GameConfig.zoom_lerp_speed)
+	# while moving, check if velocity > 60% of move speed,
+	#	then zoom in
+	if abs(get_parent().velocity.x) >= GameConfig.move_speed * 0.6:
+		target_zoom = zoom_out
+		target_speed = GameConfig.zoom_lerp_speed
+		# Reset the idle timer here since this is where you start moving again
+		idle_timer = 0.0
+	# else if idle,
+	# 	add to the timer
 	elif get_parent().velocity.x == 0:
 		idle_timer += delta
-
+	
 	# Handle Zoom In for idleness
-	if idle_timer > GameConfig.zoom_in_wait && zoom < zoom_in:
-		zoom = zoom.lerp(zoom_in, GameConfig.zoom_lerp_speed)
-
-	# FIXME: refactor -> better way to implement below
-	## every frame, regardless of state, ease actual zoom toward whatever target is currently set
-	#camera.zoom = camera.zoom.lerp(target_zoom, zoom_lerp_speed * delta)
+	# if idle for long enough, then zoom in
+	if idle_timer > GameConfig.zoom_in_wait:
+		target_zoom = zoom_in
+		target_speed = GameConfig.zoom_lerp_speed
+	
+	# Apply the zoom
+	zoom = zoom.lerp(target_zoom, target_speed * delta)
--- scripts/game_config.gd
+++ scripts/game_config.gd
@@ -15,9 +15,9 @@ var number_of_jumps = 2.0
 
 # Camera Configs
 var zoom_in = 2.8
-var initial_lerp_speed = 0.03
-var zoom_out = 2.0
-var zoom_lerp_speed = 0.1
-var zoom_in_wait = 0.0
-var chase_vertical_wait = 0.0
+var initial_lerp_speed = 1.875
+var zoom_out = 1.7
+var zoom_lerp_speed = 2.2
+var zoom_in_wait = 5.0
+var chase_vertical_wait = 1.0
 var chase_lerp_speed = 1.0
